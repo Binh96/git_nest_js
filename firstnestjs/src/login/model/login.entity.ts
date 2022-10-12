16de94fda69e5d8da0753afadb6c534451559ab1
binhphan <phanxuanbinh96@gmail.com>
2022-10-12 22:32:46 +0700

    [UPDATE] entity login

--- firstnestjs/src/login/model/login.entity.ts
+++ firstnestjs/src/login/model/login.entity.ts
@@ -1,7 +1,18 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Login {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({name: 'user_name'})
+    username: string;
+
+    @Column({name: 'password'})
+    password: string;
+
+    constructor(username: string, password: string){
+        this.username = username;
+        this.password = password;
+    }
 }
